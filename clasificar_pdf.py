1870f07cb7e1e8f55cbff91517cb68f71139f712
AlexhpSENATI <1510696@senati.pe>
2025-06-02 20:27:06 -0500

    subiendo modelo de pdf limpieza

--- clasificar_pdf.py
+++ clasificar_pdf.py
@@ -1,10 +1,8 @@
 import fitz  # PyMuPDF
 import joblib
 
-# Cargar modelo entrenado
 modelo = joblib.load("modelo_entrenado.pkl")
 
-# Función para extraer texto de un PDF
 def extraer_texto_pdf(ruta_pdf):
     texto = ""
     doc = fitz.open(ruta_pdf)
@@ -12,13 +10,11 @@ def extraer_texto_pdf(ruta_pdf):
         texto += pagina.get_text()
     return texto
 
-# Ruta del PDF a clasificar
-ruta_pdf = "areah.pdf"  # cambia esto por el nombre de tu archivo
+ruta_pdf = "areah.pdf"  
 
 # Extraer texto del PDF
 texto_extraido = extraer_texto_pdf(ruta_pdf)
 
-# Clasificar el texto con el modelo
 prediccion = modelo.predict([texto_extraido])
 
 # Mostrar resultado
