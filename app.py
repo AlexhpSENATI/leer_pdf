322d44184006803d75bcd7f5f189d3c05b729cf6
AlexhpSENATI <1510696@senati.pe>
2025-06-04 22:29:20 -0500

    error en web

--- app.py
+++ app.py
@@ -1,17 +1,54 @@
 from flask import Flask, render_template, request
-from scripts.clasificar_pdf import extraer_texto_pdf  # Asegúrate que esta función reciba un número
+import fitz  # PyMuPDF
+import joblib
+import numpy as np
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-@app.route('/')
+# Cargar modelo entrenado
+modelo = joblib.load("models/modelo_entrenado.pkl")
+
+def extraer_texto_pdf(ruta_pdf):
+    texto = ""
+    doc = fitz.open(ruta_pdf)
+    for pagina in doc:
+        texto += pagina.get_text()
+    return texto
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/predecir', methods=['POST'])  
-def predecir():
-    entrada = request.form['entrada']
-    resultado = extraer_texto_pdf(float(entrada))  
-    return render_template('index.html', resultado=resultado)
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'pdf_file' not in request.files:
+        return "No se subió ningún archivo", 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    archivo = request.files['pdf_file']
+    if archivo.filename == '':
+        return "Nombre de archivo vacío", 400
+
+    
+
+
+# from flask import Flask, render_template, request
+# from scripts.clasificar_pdf import extraer_texto_pdf  # Asegúrate que esta función reciba un número
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/predecir', methods=['POST'])  
+# def predecir():
+#     entrada = request.form['entrada']
+#     resultado = extraer_texto_pdf(float(entrada))  
+#     return render_template('index.html', resultado=resultado)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
